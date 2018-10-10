715f111f45b70581d650ef689947fb79c8d36024
JyunS <ko94deveric@gmail.com>
2018-10-10 16:23:48 +0900

    make DataReader Controller

---
+++ app/Http/Controllers/DataReader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DataReader extends Controller
+{ 
+
+    public function construct() {
+
+    }
+
+    public function excelReader () {
+
+    }
+
+    public function dataHandle() {
+
+    }
+}
